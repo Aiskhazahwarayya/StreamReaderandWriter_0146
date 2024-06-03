61af6729b91ffe2693205aac081f06a5f61d718c
Aiskhazahwarayya <aiskharayya307@gmail.com>
2024-06-03 10:26:40 +0700

    membuka file dan menujukanke sebuah nama file setelah selesai mengisi file baris keluar

--- IoFileWithPath/IoFileWithPath.cpp
+++ IoFileWithPath/IoFileWithPath.cpp
@@ -6,6 +6,16 @@ using namespace std;
 int main() {
     string baris;
     string NamaFile;
+
+    cout << "Masukan Nama File : ";
+    cin >> NamaFile;
+
+
+    ofstream outfile;
+    outfile.open(NamaFile + ".txt", ios::out);
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+
 }
 
 
