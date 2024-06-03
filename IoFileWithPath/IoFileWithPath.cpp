819e6319ddeebf26be095746ed28975214b04a02
Aiskhazahwarayya <aiskharayya307@gmail.com>
2024-06-03 10:27:53 +0700

    unlimited loop untuk menulis

--- IoFileWithPath/IoFileWithPath.cpp
+++ IoFileWithPath/IoFileWithPath.cpp
@@ -10,11 +10,16 @@ int main() {
     cout << "Masukan Nama File : ";
     cin >> NamaFile;
 
-
     ofstream outfile;
     outfile.open(NamaFile + ".txt", ios::out);
     cout << ">= Menulis file, \'q\' untuk keluar" << endl;
 
+    while (true) {
+        cout << "- ";
+        getline(cin, baris);
+        if (baris == "q") break;
+        outfile << baris << endl;
+    }
 
 }
 
